eeddbdc08774cb83787648d6e2487e5c40b60723
Sawyer X <xsawyerx@cpan.org>
2012-04-03 12:25:29 +0200

    provide object skeleton with checks attribute

--- lib/Juno.pm
+++ lib/Juno.pm
@@ -2,4 +2,15 @@ use strict;
 use warnings;
 package Juno;
 
+use Any::Moose;
+use namespace::autoclean;
+
+has checks => (
+    is       => 'ro',
+    isa      => 'HashRef',
+    required => 1,
+);
+
+__PACKAGE__->meta->make_immutable;
+
 1;
