23df21976d10f8ee2e3da0fdb671071ceb27bdeb
Sawyer X <xsawyerx@cpan.org>
2012-04-04 09:19:14 +0300

    add documentation for the role

--- lib/Juno/Role/Checker.pm
+++ lib/Juno/Role/Checker.pm
@@ -11,3 +11,29 @@ use namespace::autoclean;
 
 1;
 
+__END__
+
+=head1 DESCRIPTION
+
+This role provides Juno checkers with basic functionality they all share.
+
+=head1 ATTRIBUTES
+
+=head2 on_success
+
+A callback for when an action succeeded.
+
+=head2 on_fail
+
+A callback for when an action failed.
+
+=head2 on_result
+
+A callback to catch any result.
+
+This is useful if you have your own logic and don't count on the checker
+to decide if something is successful or not.
+
+Suppose you run the HTTP check and you have a special setup where 403 Forbidden
+is actually a correct result.
+
