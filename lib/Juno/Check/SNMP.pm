243c8964257a6918bbd15617bb426a891d03d15a
Sawyer X <xsawyerx@cpan.org>
2012-04-09 22:26:33 +0300

    indentation

--- lib/Juno/Check/SNMP.pm
+++ lib/Juno/Check/SNMP.pm
@@ -9,45 +9,45 @@ use namespace::autoclean;
 
 with 'Juno::Role::Check';
 
-has 'hostname'  => (
-    is => 'ro', 
-    isa => 'Str',       
+has hostname => (
+    is       => 'ro',
+    isa      => 'Str',
     required => 1,
 );
 
-has 'community' => (
-    is => 'ro', 
-    isa => 'Str',       
+has community => (
+    is       => 'ro',
+    isa      => 'Str',
     required => 1
 );
 
-has 'version'   => (
-    is => 'ro', 
-    isa => 'Int',       
+has version => (
+    is       => 'ro',
+    isa      => 'Int',
     required => 1
 );
 
-has 'oid'       => (
-    is => 'ro', 
-    isa => 'Str',       
+has oid => (
+    is       => 'ro',
+    isa      => 'Str',
     required => 1
 );
 
-has 'session'   => (
-    is => 'ro', 
-    isa => 'Net::SNMP', 
+has session => (
+    is         => 'ro',
+    isa        => 'Net::SNMP',
     lazy_build => 1
 );
 
 sub _build_session {
     my $self = shift;
 
-    my ($session, $error) = Net::SNMP->session (
-                        -hostname       => $self->hostname,
-                        -community      => $self->community,
-                        -version        => $self->version,
-                        -nonblocking    =>  1,
-                        );
+    my ( $session, $error ) = Net::SNMP->session(
+        -hostname    => $self->hostname,
+        -community   => $self->community,
+        -version     => $self->version,
+        -nonblocking => 1,
+    );
 
     defined $session or die "ERROR creating session: $error.\n";
 
@@ -55,18 +55,18 @@ sub _build_session {
 }
 
 sub check {
-        my $self = shift;
+    my $self = shift;
 
-        $self->has_on_before
-            and $self->on_before($self);
+    $self->has_on_before
+        and $self->on_before($self);
 
-        $self->session->get_request(
-            -varbindlist    =>  [ $self->oid ],
-            -callback       => sub {
-                    $self->has_on_result
-                        and $self->on_result->($self, @_);
-            },
-        );
+    $self->session->get_request(
+        -varbindlist    => [ $self->oid ],
+        -callback       => sub {
+            $self->has_on_result
+                and $self->on_result->( $self, @_ );
+        },
+    );
 }
 
 __PACKAGE__->meta->make_immutable;
